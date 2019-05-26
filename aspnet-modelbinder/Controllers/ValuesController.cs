e3816ef6e3d5bcc015d7a9e53c8b26444d11ea1f
Dvlp <biola.gold@gmail.com>
2019-05-26 09:13:38 +0100

    [Prolifik]

---
+++ aspnet-modelbinder/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnet_modelbinder.Controllers.Web.ModelBinder;
+
+namespace aspnet_modelbinder.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpPost]
+        public IActionResult CreateCustomer( [ModelBinder(typeof(JsonWithFilesFormDataModelBinder))]Customer customer)
+        {
+            byte[] passport = new byte[customer.Passport.Length]; 
+            var stream = customer.Passport.OpenReadStream();
+            stream.Read(passport);
+            return Ok();
+        }
+    }
+}
---
+++ aspnet-modelbinder/Models/Customer.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Http;
+
+public class Customer
+{
+    public string LastName {get;set;}
+    public string FirstName {get;set;}
+    public IFormFile Passport {get;set;}
+}
